8a5a6ac78477e39894fb8be92f11ae4082c3ff14
xyx98 <787633258@qq.com>
2026-01-27 16:14:20 +0800

    metrics:add dssim support.

--- metrics/__init__.py
+++ metrics/__init__.py
@@ -2,3 +2,4 @@ from .ssim import ssim
 from .vmaf import vmaf,vmaf_model
 from .vmafcuda import vmafcuda
 from .gsmd import gmsd
+from .dssim import dssim
---
+++ metrics/dssim.py
@@ -0,0 +1,59 @@
+from .base import metric
+import re
+import csv
+from ..utils import MEAN
+import statistics
+
+class dssim(metric):
+    def __init__(self,charset:str="utf-8",mean_mode:MEAN=MEAN.average):
+        self.provide=["dssim","ssim"]
+        self.name="dssim"
+        self.charset=charset
+        self.mean_mode=mean_mode
+    
+    def genscript(self, orgscript:str,dstpath:str) -> str:
+        with open(orgscript,'r',encoding=self.charset) as file:
+            script=file.read()
+        
+        rex=re.compile(r"(.+)\.set_output\(\)")
+        
+        match=rex.search(script)
+        clip=match.group(1)
+        
+        script=rex.sub("",script)
+        script+=f'\nimport xvs\n'
+        script+=f'dst=core.lsmas.LWLibavSource(r"{dstpath}",cache=False)\n'
+        script+=f'dst=core.resize.Spline36(dst,{clip}.width,{clip}.height,format=vs.RGB24,matrix_in_s="709")\n'
+        script+=f'{clip}=core.resize.Spline36({clip},format=vs.RGB24,matrix_in_s="709")\n'
+        script+=f'last=core.dssim.dssim({clip},dst)\n'
+        script+=f'last=xvs.props2csv(last,["dssim_score","dssim_ssim"],["dssim","ssim"],"{self.infopath(dstpath)}")\n'
+        script+=f'last.set_output()'
+        return script
+    
+    def infopath(self,dstpath:str) -> str:
+        return f"{dstpath}_{self.name}.csv"
+    
+    def getresult(self, infopath:str) -> dict[str,float|int]:
+        with open(infopath,"r") as file:
+            data=[i for i in csv.DictReader(file,delimiter="\t")]
+
+        if self.mean_mode==MEAN.average:
+            mean=statistics.fmean
+        elif self.mean_mode==MEAN.harmonic:
+            mean=statistics.harmonic_mean
+        elif self.mean_mode==MEAN.geometric:
+            mean=statistics.geometric_mean
+        elif self.mean_mode==MEAN.quadratic:
+            mean=lambda i: (sum(map(lambda x: x**2),i)/len(i))**0.5
+
+        dssim=[]
+        ssim=[]
+
+        for line in data:
+            dssim.append(float(line['dssim']))
+            ssim.append(float(line['ssim']))
+        
+        return {
+            "dssim":mean(dssim),
+            "ssim":mean(ssim),
+        }
